b56ca01d96889a1fc90bb68de9f2d42c347a9bb9
zMynx <lior.dux@develeap.com>
2024-03-24 15:53:21 +0200

    added 2 old script ive adjusted to better create modules for terraform with docs and examples

---
+++ scripts/mkdocs.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+modules_dir="modules"
+module_name=$1
+mkexample() {
+  if [ -z "$1" ]; then
+    echo "Module name not provided."
+    return 1
+  fi
+
+  module_name="$1"
+  module_path="$modules_dir/$module_name"
+  example_file="$module_path/EXAMPLE.md"
+
+  if [ -f "$example_file" ]; then
+    echo "EXAMPLE.md file already exists in the module directory."
+    return 1
+  fi
+
+  # init repo if not initiate already (required for docs)
+  cd $module_path; terraform init; cd -
+  # Generate example configuration using terraform-docs
+  example_config=$(terraform-docs tfvars hcl "$module_path")
+
+  # Create the example.md file with the desired structure
+  cat <<EOF >"$example_file"
+module "$module_name" {
+$(
+  echo "source = \"../../path/to/this/module\"" | awk '{print "  " $0}'
+)
+
+$(
+  echo "$example_config" | awk '{print "  " $0}'
+)
+}
+EOF
+}
+
+mkdocs() {
+  if [ -z "$1" ]; then
+    echo "Module name not provided."
+    return 1
+  fi
+  
+  module_name="$1"
+  module_path="$modules_dir/$module_name"
+  readme_file="$module_path/README.md"
+  
+  if [ -f "$readme_file" ]; then
+    echo "README.md file already exists in the module directory."
+    return 1
+  fi
+  
+  # Set header
+  echo "# Module \"$module_name\"" >> "$readme_file"
+  echo "---" >> "$readme_file"
+  
+  cat <<EOFTABLE >>"$readme_file"
+- [Module \"$module_name\"](#$module_name)
+- [Usage](#usage)
+- [Docs](#docs)
+  - [Requirements](#requirements)
+  - [Providers](#providers)
+  - [Modules](#modules)
+  - [Resources](#resources)
+  - [Inputs](#inputs)
+  - [Outputs](#outputs)
+- [Graph](#graph)"
+EOFTABLE
+  echo "" >> "$readme_file"
+
+  # Set header
+  echo "# Usage" >> "$readme_file"
+  echo "---" >> "$readme_file"
+  echo "Paste the following snipped into your code to use the module." >> "$readme_file"
+  
+  # Paste in the example in code block
+  example_file="$module_path/EXAMPLE.md"
+  echo '```terraform' >> "$readme_file"
+  cat $example_file >> "$readme_file"
+  echo '```' >> "$readme_file"
+  
+  echo "" >> "$readme_file"
+  echo "Run <kbd>terraform init</kbd> to initialize the module." >> "$readme_file"
+  echo "" >> "$readme_file"
+  echo "Run <kbd>terraform plan</kbd> to create the execution plan." >> "$readme_file"
+  echo "" >> "$readme_file"
+  echo "Run <kbd>terraform apply</kbd> to create the resources." >> "$readme_file"
+  echo "" >> "$readme_file"
+  echo "Run <kbd>terraform destroy</kbd> to destroy the resources." >> "$readme_file"
+  echo "" >> "$readme_file"
+
+  # Add a separator between the generated documentation and the Terraform graph
+  echo -e "\n---\n" >> "$readme_file"
+  
+  # Set header
+  echo "# Docs" >> "$readme_file"
+  echo "---" >> "$readme_file"
+  
+  # Run terraform-docs to generate module documentation and append it to README.md
+  terraform-docs markdown "$module_path" >> "$readme_file"
+  
+  # Add a separator between the generated documentation and the Terraform graph
+  echo -e "\n---\n" >> "$readme_file"
+  
+  # Set header
+  echo "# Graph" >> "$readme_file"
+  echo "---" >> "$readme_file"
+  
+  # Run terraform graph and generate a GRAPH.svg
+  graph_file="./GRAPH.svg"
+  cd $module_path
+  terraform graph -draw-cycles | dot -Tsvg >> "$graph_file"
+  
+  # deletes init leftovers
+  rm -rf .terraform && rm .terraform.lock.hcl
+  cd -
+
+  echo '<img src="./GRAPH.svg" alt="" />' >> "$readme_file"
+}
+
+mkexample $module_name
+mkdocs $module_name
---
+++ scripts/mkmodule.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+mkmodule() {
+  if [ -z "$1" ]; then
+    echo "Module name not provided."
+    return 1
+  fi
+  
+  modules_dir="modules"
+  module_name=$1
+  module_path="$modules_dir"/"$module_name"
+  mkdir -p $module_path || return 1
+  
+  # Create a versions template for this module
+  cat <<TF >"$module_path/0-versions.tf"
+terraform {
+  required_version = ">= $(terraform --version | head -1 | awk '{print $2}' | sed 's/v//' | sed 's/+.*//')"
+}
+TF
+
+  # Create a main.tf file for this module
+  cat <<TF >"$module_path/1-main.tf"
+module "$module_name" {
+
+}
+TF
+
+  # Create a outputs.tf file for this module
+  cat <<TF >"$module_path/2-outputs.tf"
+output "$module_name" {
+  value = module.*
+}
+TF
+
+  # Create a locals.tf file for this module
+  cat <<TF >"$module_path/3-locals.tf"  
+locals {}
+TF
+
+  # Create a data_sources.tf file for this module
+  cat <<TF >"$module_path/4-data_sources.tf"
+data "example" {}
+TF
+
+  # Create a variables.tf file for this module
+  cat <<TF >"$module_path/5-variables.tf"
+variable "example" {
+  type = string
+}
+TF
+}
+
+mkmodule $1
